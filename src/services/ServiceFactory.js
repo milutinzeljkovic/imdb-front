11d712cfa61e21fe12b9616b727e551f5960ac2f
= <=>
2019-10-15 13:03:21 +0200

    changes in naming

--- src/services/ServiceFactory.js
+++ src/services/ServiceFactory.js
@@ -1,9 +1,13 @@
 import UsersService from './UsersService';
+import MoviesService from './MoviesService';
 
 const usersService = new UsersService();
+const moviesService = new MoviesService();
 
 const services = {
     users: usersService,
+    movies: moviesService
+
 };
 
 export const ServiceFactory = {
